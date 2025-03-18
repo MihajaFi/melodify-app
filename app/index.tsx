96b0819bce308c90e7c23d960ee86fa9bac265d5
MihajaFi <hei.fifaliana@gmail.com>
2025-03-18 15:31:14 +0300

    feat: add playlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen, PlayerScreen, PlaylistScreen } from "@/screen";
-import { RootStackParamList } from '@/types';
-import * as SplashScreen from 'expo-splash-screen';
-const Stack = createStackNavigator<RootStackParamList>(); 
+import { RootStackParamList } from "@/types";
+import * as SplashScreen from "expo-splash-screen";
+const Stack = createStackNavigator<RootStackParamList>();
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 2000);
 
 export default function Index() {
   return (
-    
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen  name="Player" component={PlayerScreen} />
-        <Stack.Screen name="Playlist" component={PlaylistScreen} />
-      </Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Player" component={PlayerScreen} />
+      <Stack.Screen name="Playlist" component={PlaylistScreen} />
+    </Stack.Navigator>
   );
 }
